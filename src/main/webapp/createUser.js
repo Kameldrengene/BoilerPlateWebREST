0e2a30f096d8c5750bfbc515cd357070652ebe88
Mark <s174881@student.dtu.dk>
2020-05-10 11:31:51 +0200

    no message

--- src/main/webapp/createUser.js
+++ src/main/webapp/createUser.js
@@ -6,12 +6,14 @@ async function createUser() {
         pass: document.getElementById("pass").value,
         roles: ["null", "null", "null", "null"]
     };
+    var nrOfRoles = 0;
     for (var x = 1; x < 5; x++) {
         if (document.getElementById("role" + x).checked && document.getElementById("role" + x).value != "null") {
             user.roles[x - 1] = document.getElementById("role" + x).value;
+            nrOfRoles++;
         }
     }
-    if (user.username != "" && user.ini != "" && user.cpr != "" && user.pass != "") {
+    if (user.username != "" && user.ini != "" && user.cpr != "" && user.pass != "" && nrOfRoles != 0) {
         if (confirm("Are you sure you want to create user?")) {
             var response = await fetch("/BoilerPlate_war_exploded/rest/live/mysql_json/createUser/" + user.username + "/" + user.ini + "/" + user.cpr + "/" + user.pass + "/" + user.roles[0] + "/" + user.roles[1] + "/" + user.roles[2] + "/" + user.roles[3]);
             console.log(JSON.stringify(user));
@@ -21,8 +23,12 @@ async function createUser() {
         }
         console.log(JSON.stringify(user));
         console.log(response.text());
-    } else
+    } else if(nrOfRoles > 0){
         alert("Please fill out all columns");
+    } else {
+        alert("Please select a role")
+    }
+
 }
 
 function load_users(){
