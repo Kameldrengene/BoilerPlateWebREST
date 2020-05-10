f00f5075111b51d40898307f5ae7630667199ec9
Mark <s174881@student.dtu.dk>
2020-05-10 16:44:49 +0200

    no message

--- src/main/java/WebService.java
+++ src/main/java/WebService.java
@@ -4,57 +4,10 @@ import dto.UserDTO;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 import java.util.List;
 
 @Path("live")
 public class WebService {
-    MySQLConnector conn = MySQLConnector.getInstance();
-
-    @Path("hello")
-    @GET
-    public String getTest() {
-        return "Hello World";
-    }
-
-    @Path("json")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Hello getHelloJson() {
-        return new Hello();
-    }
-
-    @Path("mysql_databases_nr")
-    @GET
-    public String getNrOfDatabases() throws SQLException {
-        Connection connection = conn.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SHOW DATABASES ");
-        int size = 0;
-        while (resultSet.next()) {
-            size++;
-        }
-        return Integer.toString(size);
-
-    }
-
-
-    @Path("mysql_databases/{i}")
-    @GET
-    public String getDatabases(@PathParam("i") int i) throws SQLException {
-        Connection connection = conn.getConnection();
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery("SHOW DATABASES ");
-        int j = i;
-        while (resultSet.next() && j != 0) {
-            j--;
-        }
-        return resultSet.getString(1);
-    }
-
     @Path("mysql_json")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
